e6441fd6562cf6339f672e4baef69d485aa2e1ed
Anthony Giles <omgliekpie@gmail.com>
2020-01-18 15:19:43 -0700

    start making a function to give a random message each minute

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -4,6 +4,10 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+    if (seconds == 1) {
+        changeMessage();
+    }
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
@@ -12,12 +16,16 @@ function setDate() {
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+     
+}
 
-    console.log(hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds')
+function changeMessage() {
+    console.log('trying to change message');
 }
 
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const message = document.querySelector('p');
 
 setInterval(setDate, 1000);
