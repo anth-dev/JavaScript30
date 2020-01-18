59ffc455b1602e200393b55569d82f9ac21f1389
Anthony Giles <omgliekpie@gmail.com>
2020-01-18 14:01:42 -0700

    add hour hand functionality

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -4,15 +4,20 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds + ' seconds');
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    console.log(minutes + ' minutes');
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    console.log(hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds')
 }
 
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 setInterval(setDate, 1000);
